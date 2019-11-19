bc099c69e614ab42971d8873e7d9541e19e8109c
江涛 <tao.jiang02@hand-china.com>
2019-11-19 17:14:50 +0800

    sb saas-loader

--- curriculum-vitae/src/App.vue
+++ curriculum-vitae/src/App.vue
@@ -33,7 +33,7 @@ export default {
 }
 </script>
 
-<style >
+<style lang="scss">
   #app {
     font-family: 'Avenir', Helvetica, Arial, sans-serif;
     -webkit-font-smoothing: antialiased;
@@ -41,9 +41,11 @@ export default {
     text-align: center;
     color: #2c3e50;
     margin-top: 60px;
-  }
-  .title {
-    font-size: 46px;
-    font-weight: bolder;
+
+    .title {
+      font-size: 46px;
+      font-weight: bolder;
+    }
+
   }
 </style>
