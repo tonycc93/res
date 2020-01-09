7ae201ed88d16f8af34eda217d70ca41a025a042
tonycc93 <jiangtao_tj93@163.com>
2020-01-09 16:17:19 +0800

    闭包 (#1)

---
+++ curriculum-vitae/src/components/collection/closure-demo.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="closure" ref="closure">康康闭包</div>
+</template>
+
+<script>
+export default {
+  name: 'closure-demo',
+  methods: {
+    clo (e) {
+      return (e) => {
+        console.log(this.refs.closure)
+        console.log(e)
+      }
+    }
+  },
+  mounted () {
+    (this.clo())(1) // 大概是这样吧
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
